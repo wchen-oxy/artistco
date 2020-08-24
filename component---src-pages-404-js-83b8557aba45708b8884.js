c5034e07c5b9154e5410a3fc0334e83dd3fbe0aa
William S Chen <blackjamx5@hotmail.com>
2020-08-24 15:23:19 -0700

    Updates

---
+++ component---src-pages-404-js-83b8557aba45708b8884.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a);t.default=function(){return l.a.createElement("div",{stlye:{width:"100%",height:"100vh"}},l.a.createElement("h4",null,"404 Page not found"),l.a.createElement("p",null,"You mad lad, this isn't a page"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-83b8557aba45708b8884.js.map

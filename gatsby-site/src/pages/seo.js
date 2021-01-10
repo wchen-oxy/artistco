954e9a325b593363df017b0096f840c2714c9c54
William S Chen <blackjamx5@hotmail.com>
2021-01-10 15:57:18 -0800

    adjusted image metaData

--- gatsby-site/src/pages/seo.js
+++ gatsby-site/src/pages/seo.js
@@ -22,12 +22,13 @@ const SEO = ({ title, description, image, article }) => {
     title: title || defaultTitle,
     description: description || defaultDescription,
     image: `${siteUrl}${image || defaultImage}`,
+    rawUrl: image || defaultImage,
     url: `${siteUrl}${pathname}`,
   }
    return (
     <Helmet title={seo.title} titleTemplate={titleTemplate}>
       <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
+      <meta name="image" content={seo.rawUrl} />
       <link rel="icon" type="image/png" href={temp_icon}  sizes="16x16" />
       {seo.url && <meta property="og:url" content={seo.url} />}
       {(article ? true : null) && <meta property="og:type" content="article" />}
@@ -35,7 +36,7 @@ const SEO = ({ title, description, image, article }) => {
       {seo.description && (
         <meta property="og:description" content={seo.description} />
       )}
-      {seo.image && <meta property="og:image" content={seo.image} />}
+      {seo.image && <meta property="og:image" content={seo.rawUrl} />}
     </Helmet>
   )
 }
